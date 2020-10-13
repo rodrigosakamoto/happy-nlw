c15028a6e90c5affe94252a75a1641f837cc5e8c
rodrigosakamoto <rodosakamoto@gmail.com>
2020-10-13 16:52:02 -0300

    feat(backend): create orphanages, list orphanages

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+
+import { getRepository } from 'typeorm';
+import Orphanages from '../models/Orphanages';
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanages);
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  },
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import './database/connection';
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => res.json({ message: 'hello World' }));
+app.use(routes);
 
 app.listen(3333);
