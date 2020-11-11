69aaa8ed667c8efbf5bda80f4be2edebd8412f90
rodrigosakamoto <rodosakamoto@gmail.com>
2020-11-11 09:20:47 -0300

    feat: restructuring landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -4,25 +4,39 @@ import { Link } from 'react-router-dom';
 
 import logoImg from '../../images/logo.svg';
 
-import { Container, ContentWrapper, Location } from './styles';
+import {
+  Container,
+  ContentWrapper,
+  Header,
+  HeaderContainer,
+  Content,
+  Location,
+} from './styles';
 
 const Landing: React.FC = () => {
   return (
     <Container>
       <ContentWrapper>
-        <img src={logoImg} alt="" />
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-        <Location>
-          <strong>Mogi das Cruzes</strong>
-          <span>São Paulo</span>
-        </Location>
+        <Header>
+          <HeaderContainer>
+            <img src={logoImg} alt="happy" />
+            <Location>
+              <strong>Mogi das Cruzes</strong>
+              <span>São Paulo</span>
+            </Location>
+          </HeaderContainer>
+          <Link to="/login">Acesso restrito</Link>
+        </Header>
 
-        <Link to="/app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </Link>
+        <Content>
+          <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+          <Link to="/app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+          </Link>
+        </Content>
       </ContentWrapper>
     </Container>
   );
--- web/src/pages/Landing/styles.ts
+++ web/src/pages/Landing/styles.ts
@@ -9,11 +9,80 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
 
-  a {
+export const ContentWrapper = styled.div`
+  width: 100%;
+  max-width: 1100px;
+
+  height: 100%;
+  max-height: 680px;
+
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+
+  background: url(${landing}) no-repeat 80% center;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > a {
+    width: 222px;
+    height: 56px;
+    border-radius: 20px;
+    text-decoration: none;
+    background: #12d4e0;
+    color: #ffffff;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 20px;
+    font-weight: 800;
+
+    &:hover {
+      background: #96feff;
+      color: #15c3d6;
+    }
+  }
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  position: relative;
+  width: 100%;
+
+  main {
+    max-width: 350px;
+
+    h1 {
+      font-size: 76px;
+      font-weight: 900;
+      line-height: 70px;
+    }
+
+    p {
+      margin-top: 40px;
+      font-size: 24px;
+      line-height: 34px;
+    }
+  }
+
+  > a {
     position: absolute;
-    right: 0;
     bottom: 0;
+    right: 0;
 
     width: 80px;
     height: 80px;
@@ -32,52 +101,16 @@ export const Container = styled.div`
   }
 `;
 
-export const ContentWrapper = styled.div`
-  position: relative;
-
-  width: 100%;
-  max-width: 1100px;
-
-  height: 100%;
-  max-height: 680px;
-
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  justify-content: space-between;
-
-  background: url(${landing}) no-repeat 80% center;
-  main {
-    max-width: 350px;
-
-    h1 {
-      font-size: 76px;
-      font-weight: 900;
-      line-height: 70px;
-    }
-
-    p {
-      margin-top: 40px;
-      font-size: 24px;
-      line-height: 34px;
-    }
-  }
-`;
-
 export const Location = styled.div`
-  position: absolute;
-  right: 0;
-  top: 0;
-
+  margin-left: 64px;
   font-size: 24px;
   line-height: 34px;
 
   display: flex;
   flex-direction: column;
 
-  text-align: right;
-
   strong {
     font-weight: 800;
+    text-align: right;
   }
 `;
